e68c95d1a680d5f1e50cc4fb74bacbad7356cb31
Joonyoung Park <joonyoungpark33@gmail.com>
2020-11-09 21:08:47 +0900

    Added this and arguments locs.

--- src/js/sample_analyses/dlint/DynamicShortcut.js
+++ src/js/sample_analyses/dlint/DynamicShortcut.js
@@ -44,8 +44,8 @@
           var ty = typeof val;
           if(val !== null && ["object", "function"].includes(ty)) {
             const loc = iidMap[iid][1] + ":" + J$.____context.tracePartition.ToString();
+            J$.____heap[loc] = val;
             if(!J$.____refMap.has(val)) {
-              J$.____heap[loc] = val;
               J$.____refMap.set(val, loc);
             }
 
@@ -54,10 +54,9 @@
               Object.defineProperty(val, "____Construct", { value: +iidMap[iid][2], writable: true, configruable: true });
               Object.defineProperty(val, "____Scope", { value: J$.____context.env[0], writable: true, configruable: true });
               let prototype = val.prototype;
-              const flag = !J$.____refMap.has(prototype);
-              if(flag) {
-                let loc = iidMap[iid][3] + ":" + J$.____context.tracePartition.ToString();
-                J$.____heap[loc] = prototype;
+              let loc = iidMap[iid][3] + ":" + J$.____context.tracePartition.ToString();
+              J$.____heap[loc] = prototype;
+              if(!J$.____refMap.has(prototype)) {
                 J$.____refMap.set(prototype, loc);
               }
             }
@@ -69,10 +68,11 @@
           let prop = "Call";
           if(isConstructor) {
             prop = "Construct";
-            if(iidMap[iid][prop] === undefined) {
-              process.stdout.write(iid.toString() + "\n");
-            }
-            J$.____isConstructor = iidMap[iid][prop][4] + ":" + J$.____context.tracePartition.ToString();
+            J$.____isConstructor = iidMap[iid][prop][5] + ":" + J$.____context.tracePartition.ToString();
+          }
+          J$.____argumentsLoc = iidMap[iid][prop][4] + ":" + J$.____context.tracePartition.ToString();
+          if(iid === 9153) {
+            throw new Error(J$.____argumentsLoc);
           }
           if(iidMap[iid][prop] && iidMap[iid][prop].length > 1) {
             J$.____context.env.unshift(iidMap[iid][prop][1] + ":" + J$.____context.tracePartition.ToString());
@@ -93,23 +93,25 @@
 
         this.invokeFun = function (iid, f, base, args, result, isConstructor, isMethod) {
           J$.____isConstructor = undefined;
+          J$.____argumentsLoc = undefined;
           let prop = "Call";
           if(isConstructor) {
             prop = "Construct";
           }
           if (result !== null && ["object", "function"].indexOf(typeof result) >= 0) {
-            const flag = !J$.____refMap.has(result);
-            if(flag) {
+            if(!J$.____refMap.has(result)) {
               let fid;
               if (isConstructor) {
                 fid = f.____Construct;
               } else {
                 fid = f.____Call;
               }
+
               let loc = "#" + fid + ":" + J$.____context.tracePartition.ToString();
               if(fid === -102) {
                 fid = "-102[12]";
               }
+
               J$.____heap[loc] = result;
               J$.____refMap.set(result, loc);
             }
@@ -166,9 +168,17 @@
         this.functionEnter = function (iid, f, dis, args, getter) {
           J$.____path.push(iid);
           J$.____stack.push(iid);
+
+          // arguments
+          J$.____heap[J$.____argumentsLoc] = args;
+          if(!J$.____refMap.has(args)) {
+            J$.____refMap.set(args, J$.____argumentsLoc);
+          }
+
+          // this 
           if(J$.____isConstructor) {
+            J$.____heap[J$.____isConstructor] = dis;
             if(!J$.____refMap.has(dis)) {
-              J$.____heap[J$.____isConstructor] = dis;
               J$.____refMap.set(dis, J$.____isConstructor);
             }
           }
